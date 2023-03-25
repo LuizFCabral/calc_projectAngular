058228f966dee6c1a54c7b7ddb2c1692874f1899
Luiz Cabral <luizfelipe.cabralsouza@gmail.com>
2023-03-25 18:37:35 -0300

    :sparkles: feat: Adiciona novos componentes e estilizações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
+import { InputComponent } from './input/input.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalcComponent
+    CalcComponent,
+    InputComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
