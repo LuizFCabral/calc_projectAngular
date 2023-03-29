4ff44b37144404250be23ab8d5e8f3b2b81d06de
Luiz Cabral <luizfelipe.cabralsouza@gmail.com>
2023-03-28 21:23:12 -0300

    :sparkles: feat: Conecta os componentes

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -3,14 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
-  styleUrls: ['./calc.component.css']
+  styleUrls: ['./calc.component.css'],
 })
 export class CalcComponent {
-  num1 : number = 0;
-  num2 : number = 0;
-  sum : number = 0;
+  num1: number = 0;
+  num2: number = 0;
+  sum: number = 0;
 
-  somar(){
+  somar() {
     this.sum = this.num1 + this.num2;
+    console.log(this.sum);
+  }
+
+  getNum1(event: any) {
+    this.num1 = Number(event);
+  }
+  getNum2(event: any) {
+    this.num2 = Number(event);
   }
 }
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent {
+  value!: number;
 
+  @Output() sendValue = new EventEmitter();
+
+  onChange() {
+    this.sendValue.emit(this.value);
+  }
 }
